989c5358c65b04da5345ede1e1419594075de3a5
gonchox <gfarfan595@gmail.com>
2022-04-09 15:11:39 -0500

    Primer Commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
+
+  borrarHeroe() {
+    this.heroes = [];
+  }
+}
